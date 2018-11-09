8e0d0a0e1ba76258c9ba8cd3e4ce0be12777350c
Blake <bllpr15@gmail.com>
2018-11-09 15:13:36 -0500

    Added too methods to tictactoe.h and added a main.cpp

--- tictactoe.h
+++ tictactoe.h
@@ -10,20 +10,54 @@ class TicTacToe
 {
   public:
 
+    //initializes empty board and 0 points for 'O' and 'X'
     TicTacToe();
 
-    void insert_x(int cordinate);
+    //places a 'X' in a cordinate
+    void insert_x(int row_cord, int col_cord);
 
-    void insert_o(int cordinate);
+    //places a 'O' in a cordinate
+    void insert_o(int row_cord, int col_cord);
 
+    //resets the game board
     void resetBoard();
 
+    //prints the game board
     void printBoard();
+
+    //uses checkRow, checkColumn, and checkDiag to see if either 'O' or 'X' won
+    //returns 'O' or 'X' if win condition is met
+    char checkBoard();
+
+    //tallies win condition with +1
+    void x_won();
+
+    //tallies win condition with +1
+    void o_won();
+
+    //prints the scoreboard
+    void printScore();
 
 
   private:
-
+    
+    //contains the game board, default empty
     char board[3][3]; //this will hold the 2D board as an array, it being 3x3
+
+    //checks for win condition in rows
+    char checkRow();
+
+    //checks for win condition in columns
+    char checkColumn();
+
+    //checks for win condition in diagonals
+    char checkDiag();
+
+    //carries 'X' points
+    int x_score = 0;
+
+    //carries 'O' points
+    int o_score = 0;
 
 };
 
