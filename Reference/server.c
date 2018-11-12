75d43faaebfbb2676a0fb495dd5d53a356070628
Blake <bllpr15@gmail.com>
2018-11-12 15:50:44 -0500

    changed some wording and got rid of magic numbers

--- Reference/server.c
+++ Reference/server.c
@@ -5,42 +5,28 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define MAXBUF 256 /* MAX I/O buffer size */
-#define MAXLINE 256 /* Max text line length */
-#define LISTENQ 5 /* Second argument to listen() */
-
-/* Error message response */
 void error(char *msg)
 {
     perror(msg);
-    return 1;
+    exit(1);
 }
 
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno, clilen;
-
-     char buffer[MAXBUF]; //buffer for data to be sent or retrieved
-
-     //structs for server, client, and socket
+     char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
 
-     int response; //holds response success or failure determination value
 
-
-     //not enough arguments provided
-     if (argc < 2) 
-     {
+     int n;
+     if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
-         return 1;
+         exit(1);
      }
 
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     //socket error
      if (sockfd < 0) 
         error("ERROR opening socket");
-
-
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
@@ -48,28 +34,16 @@ int main(int argc, char *argv[])
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
-
-     //listen for client
-     listen(sockfd, LISTENQ);
-
+     listen(sockfd,5);
      clilen = sizeof(cli_addr);
      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
      if (newsockfd < 0) 
           error("ERROR on accept");
-
-     bzero(buffer, MAXBUF);
-
-     response = fread(newsockfd, buffer, MAXBUF-1);
-     if (response < 0) 
-       error("ERROR reading from socket");
-
+     bzero(buffer,256);
+     n = read(newsockfd,buffer,255);
+     if (n < 0) error("ERROR reading from socket");
      printf("Here is the message: %s\n",buffer);
-
-     response = fwrite(newsockfd,"I got your message",18);
-     if (response < 0) 
-       error("ERROR writing to socket");
-
-
+     n = write(newsockfd,"I got your message",18);
+     if (n < 0) error("ERROR writing to socket");
      return 0; 
 }
