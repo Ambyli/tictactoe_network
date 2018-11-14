1880115948a85871acd20d062e31caad0f7fbc91
Blake Price <blpr226@cs.uky.edu>
2018-11-14 12:36:40 -0500

    Adjusted client

--- client.cpp
+++ client.cpp
@@ -11,74 +11,75 @@
 /* Error message response */
 void error(char *msg)
 {
-    perror(msg);
-    return 1;
+  perror(msg);
+  exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, response;
-    
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+  int sockfd, portno, response;
 
-    char buffer[MAXBUF]; //limits how much text can be sent to the server
-    
-    //too little arguments
-    if (argc < 3) 
-    {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       return 0;
-    }
-    
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    
-    //socket error
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
 
-    server = gethostbyname(argv[1]); //retrieve host address from argument
+  char buffer[MAXBUF]; //limits how much text can be sent to the server
 
-    //if no host address was given
-    if (server == NULL) 
-    {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-
-    serv_addr.sin_port = htons(portno);
-
-    //error connecting
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-
-
-    printf("Please enter the message: ");
-    bzero(buffer, MAXBUF);
-    fgets(buffer, MAXBUF-1, stdin);
-   
-    //write to server
-    response = write(sockfd, buffer, strlen(buffer));
-
-    //nothing written error
-    if (response < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer, MAXBUF);
-
-    //read from server
-    response = read(sockfd, buffer, MAXBUF-1);
-
-    //nothing retrieved error
-    if (response < 0) 
-         error("ERROR reading from socket");
-
-    //print response
-    printf("%s\n",buffer);
+  //too little arguments
+  if (argc < 3) 
+  {
+    fprintf(stderr,"usage %s hostname port\n", argv[0]);
     return 0;
+  }
+  
+  //get port number
+  portno = atoi(argv[2]);
+  //open socket
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  //socket error
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+
+  //retrieve host address from argument
+  server = gethostbyname(argv[1]);
+  //if no host address was given
+  if (server == NULL) 
+  {
+    fprintf(stderr,"ERROR, no such host\n");
+    return 0;
+  }
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+
+  bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+  serv_addr.sin_port = htons(portno);
+
+  //error connecting
+  if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+    error("ERROR connecting");
+
+
+  printf("Please enter the message: ");
+  bzero(buffer, MAXBUF);
+  fgets(buffer, MAXBUF-1, stdin);
+
+  //write to server
+  response = write(sockfd, buffer, strlen(buffer));
+
+  //nothing written error
+  if (response < 0) 
+    error("ERROR writing to socket");
+  bzero(buffer, MAXBUF);
+
+  //read from server
+  response = read(sockfd, buffer, MAXBUF-1);
+
+  //nothing retrieved error
+  if (response < 0) 
+    error("ERROR reading from socket");
+
+  //print response
+  printf("%s\n",buffer);
+  return 0;
 }
--- server.cpp
+++ server.cpp
@@ -12,64 +12,64 @@
 /* Error message response */
 void error(char *msg)
 {
-    perror(msg);
-    return 1;
+  perror(msg);
+  return 1;
 }
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
+  int sockfd, newsockfd, portno, clilen;
 
-     char buffer[MAXBUF]; //buffer for data to be sent or retrieved
+  char buffer[MAXBUF]; //buffer for data to be sent or retrieved
 
-     //structs for server, client, and socket
-     struct sockaddr_in serv_addr, cli_addr;
+  //structs for server, client, and socket
+  struct sockaddr_in serv_addr, cli_addr;
 
-     int response; //holds response success or failure determination value
-
-
-     //not enough arguments provided
-     if (argc < 2) 
-     {
-         fprintf(stderr,"ERROR, no port provided\n");
-         return 1;
-     }
-
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     //socket error
-     if (sockfd < 0) 
-        error("ERROR opening socket");
+  int response; //holds response success or failure determination value
 
 
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+  //not enough arguments provided
+  if (argc < 2) 
+  {
+    fprintf(stderr,"ERROR, no port provided\n");
+    return 1;
+  }
 
-     //listen for client
-     listen(sockfd, LISTENQ);
-
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-
-     bzero(buffer, MAXBUF);
-
-     response = fread(newsockfd, buffer, MAXBUF-1);
-     if (response < 0) 
-       error("ERROR reading from socket");
-
-     printf("Here is the message: %s\n",buffer);
-
-     response = fwrite(newsockfd,"I got your message",18);
-     if (response < 0) 
-       error("ERROR writing to socket");
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  //socket error
+  if (sockfd < 0) 
+    error("ERROR opening socket");
 
 
-     return 0; 
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    error("ERROR on binding");
+
+  //listen for client
+  listen(sockfd, LISTENQ);
+
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+  if (newsockfd < 0) 
+    error("ERROR on accept");
+
+  bzero(buffer, MAXBUF);
+
+  response = fread(newsockfd, buffer, MAXBUF-1);
+  if (response < 0) 
+    error("ERROR reading from socket");
+
+  printf("Here is the message: %s\n",buffer);
+
+  response = fwrite(newsockfd,"I got your message",18);
+  if (response < 0) 
+    error("ERROR writing to socket");
+
+
+  return 0; 
 }
